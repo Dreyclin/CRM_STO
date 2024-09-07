45b4997b1798f81d289ca0959d1fde7282fc51f0
Maxim Sur <makarka16@gmail.com>
2024-09-07 13:56:29 +0300

    Finish auth

--- server/app.js
+++ server/app.js
@@ -30,47 +30,48 @@ app.get('/', (req, res) => {
 app.post('/login', async (req, res) => {
     console.log(res);
     try {
-        const {email, password} = req.body;
-        const user = await User.findOne({email});
-        if(!user || !(await bcrypt.compare(password, user.password))){
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(400).json({ message: 'Неверный логин/пароль или вы не зарегистрированы' });
         }
 
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
-        res.json({token, user: {id: user._id, email: user.email}})
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+        res.json({ token, user: { id: user._id, email: user.email } })
     } catch (error) {
         res.status(500).json({ message: 'Ошибка сервера' });
     }
 });
 
-app.post('/checkAuth', (req, res) => {
+app.post('/checkAuth', async (req, res) => {
     let token;
     console.log('Authorization header:', req.headers.authorization);
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization.split(' ')[1];
+        token = req.headers.authorization.split(' ')[1];
     }
-  
+
     if (!token) {
-      return res.status(401).json({ message: 'Нет токена, авторизация отклонена' });
+        return res.status(401).json({ message: 'Нет токена, авторизация отклонена' });
     }
-  
+
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-  
-      res.json({ message: 'Доступ разрешен', user: req.user });
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        const user = await User.findById(req.user.id).select('-password');
+        const { email, id } = user;
+        res.json({ message: 'Доступ разрешен', user: { id: id, email: email } });
     } catch (err) {
-      return res.status(401).json({ message: 'Неверный токен' });
+        return res.status(401).json({ message: 'Неверный токен' });
     }
 })
 
 app.post('/reg', async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    const user = await User.findOne({email: email});
-    
-    if(user) {
-        res.status(400).json({message: "Пользователь уже существует!"})
+    const user = await User.findOne({ email: email });
+
+    if (user) {
+        res.status(400).json({ message: "Пользователь уже существует!" })
     } else {
         const hashedPass = await bcrypt.hash(password, salt);
 
@@ -78,10 +79,10 @@ app.post('/reg', async (req, res) => {
             email: email,
             password: hashedPass
         })
-    
+
         await newUser.save()
-    
-        res.status(200).json({message: "Пользователь успешно зарегистрирован"})
+
+        res.status(200).json({ message: "Пользователь успешно зарегистрирован" })
     }
 
 
